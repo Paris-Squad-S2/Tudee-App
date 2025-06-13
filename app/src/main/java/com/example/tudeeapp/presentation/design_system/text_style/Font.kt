3381485b5d3ce29f97a1b2dbd2e3b0dffa90cc9f
=Muhammed Wael <=mw26200400@gmail.com>
2025-06-13 18:48:19 +0300

    feat: Add font files

---
+++ app/src/main/java/com/example/tudeeapp/presentation/design_system/text_style/Font.kt
@@ -0,0 +1,17 @@
+package com.example.tudeeapp.presentation.design_system.text_style
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.tudeeapp.R
+
+
+val ibm_plex_sans = FontFamily(
+    Font(R.font.bold , FontWeight.Bold),
+    Font(R.font.thin , FontWeight.Thin),
+    Font(R.font.medium , FontWeight.Medium),
+    Font(R.font.light , FontWeight.Light),
+    Font(R.font.regular , FontWeight.Normal),
+    Font(R.font.semi_bold , FontWeight.SemiBold),
+    Font(R.font.extra_light , FontWeight.ExtraLight)
+)
