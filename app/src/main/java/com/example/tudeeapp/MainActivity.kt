da09973a8057ffce9b25b2a442bde5b8426ad32a
=Muhammed Wael <=mw26200400@gmail.com>
2025-06-13 18:49:41 +0300

    feat: Add test preview for composable

--- app/src/main/java/com/example/tudeeapp/MainActivity.kt
+++ app/src/main/java/com/example/tudeeapp/MainActivity.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tudeeapp.presentation.design_system.theme.Theme
+import com.example.tudeeapp.presentation.design_system.theme.TudeeTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -13,5 +21,23 @@ class MainActivity : ComponentActivity() {
         setContent {
 
         }
+    }
+}
+
+@Composable
+fun TestPreview() {
+    Text(
+        text = "Hello World",
+        color = Theme.colors.text.title,
+        style = Theme.textStyle.headline.small,
+        modifier = Modifier.background(Theme.colors.primary)
+    )
+}
+
+@Preview
+@Composable
+fun Preview() {
+    TudeeTheme() {
+        TestPreview()
     }
 }
