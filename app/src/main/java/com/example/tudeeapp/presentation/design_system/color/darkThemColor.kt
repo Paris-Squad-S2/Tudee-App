c214e79dbfee4cc012773f299b255c393c30b681
=Muhammed Wael <=mw26200400@gmail.com>
2025-06-13 18:47:57 +0300

    feat: Add dark theme colors

---
+++ app/src/main/java/com/example/tudeeapp/presentation/design_system/color/darkThemColor.kt
@@ -0,0 +1,15 @@
+package com.example.tudeeapp.presentation.design_system.color
+
+import androidx.compose.ui.graphics.Color
+
+val darkThemeColors = TudeeColors(
+    primary = Color(0xFF3090BF),
+    secondary = Color(0xFFCC7851),
+    primaryVarient = Color(0xFF05202E),
+    text = TudeeTextColors(
+        title = Color(0xDEFFFFFF),
+        body = Color(0x99FFFFFF),
+        hint = Color(0x61FFFFFF),
+        stroke = Color(0x1FFFFFFF)
+    )
+)
