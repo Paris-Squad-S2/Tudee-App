0a96db6c3fdf37b71367b16f929e506a4583d592
=Muhammed Wael <=mw26200400@gmail.com>
2025-06-13 18:48:44 +0300

    feat: Add light theme colors

---
+++ app/src/main/java/com/example/tudeeapp/presentation/design_system/color/lightThemeColors.kt
@@ -0,0 +1,15 @@
+package com.example.tudeeapp.presentation.design_system.color
+
+import androidx.compose.ui.graphics.Color
+
+val lightThemeColors = TudeeColors(
+    primary = Color(0xFF49BAF2),
+    secondary = Color(0xFFF49061),
+    primaryVarient = Color(0xFFEFF9FE),
+    text = TudeeTextColors(
+        title = Color(0xDE1F1F1F),
+        body = Color(0x991F1F1F),
+        hint = Color(0x611F1F1F),
+        stroke = Color(0x1F1F1F1F)
+    )
+)
