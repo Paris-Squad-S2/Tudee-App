f8b1ef356dd3efa2bbd3537a53f14873ab050d60
Asmaa Karam <asmaakaram793@gmai.com>
2025-06-14 11:03:47 +0300

    add light colors of design system

--- app/src/main/java/com/example/tudeeapp/presentation/design_system/color/TudeeColors.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/design_system/color/TudeeColors.kt
@@ -3,13 +3,14 @@ package com.example.tudeeapp.presentation.design_system.color
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-
 data class TudeeColors(
     val primary: Color,
     val secondary: Color,
-    val primaryVarient: Color,
-//    val PrimaryGradient: Color = Color(0xFFF49061)
-    val text: TudeeTextColors
+    val primaryVariant: Color,
+    val primaryGradient: PrimaryGradient,
+    val text: TudeeTextColors,
+    val surfaceColors: SurfaceColors,
+    val status: Status
 )
 
 data class TudeeTextColors(
@@ -18,5 +19,36 @@ data class TudeeTextColors(
     val hint: Color,
     val stroke: Color
 )
+data class PrimaryGradient(
+    val colors:List<Color>
+)
 
+data class SurfaceColors(
+    val surfaceLow: Color,
+    val surface: Color,
+    val surfaceHigh: Color,
+    val onPrimaryColors:OnPrimaryColors,
+    val disable: Color,
+)
+
+data class Status(
+    val pinkAccent: Color,
+    val yellowAccent: Color,
+    val greenAccent: Color,
+    val purpleAccent: Color,
+    val error: Color,
+    val overlay: Color,
+    val emojiTint: Color,
+    val yellowVariant: Color,
+    val greenVariant: Color,
+    val purpleVariant: Color,
+    val errorVariant: Color
+)
+
+data class OnPrimaryColors(
+    val onPrimary: Color,
+    val onPrimaryCaption: Color,
+    val onPrimaryCard: Color,
+    val onPrimaryStroke: Color,
+)
 val TudeeLocalColors = staticCompositionLocalOf { lightThemeColors }
