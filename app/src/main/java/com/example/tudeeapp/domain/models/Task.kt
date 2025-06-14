c8e74cd4212d2e67c379d11fe67f1978bccaad57
IslamMagd <eslam752001.magdy54@gmail.com>
2025-06-14 17:52:16 +0300

    import LocalDate in Task.kt

--- app/src/main/java/com/example/tudeeapp/domain/models/Task.kt
+++ app/src/main/java/com/example/tudeeapp/domain/models/Task.kt
@@ -1,11 +1,13 @@
 package com.example.tudeeapp.domain.models
 
+import kotlinx.datetime.LocalDate
+
 data class Task(
     val id: Long,
     val title: String,
     val description: String,
     val priority: TaskPriority,
     val status: TaskStatus,
-    val createdDate: LocalDate, // add kotlinx dependency in the gradle ->implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
+    val createdDate: LocalDate,
     val category: TaskCategory
 )
