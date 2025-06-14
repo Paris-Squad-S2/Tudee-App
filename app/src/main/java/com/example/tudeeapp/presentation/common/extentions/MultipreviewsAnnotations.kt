19afa3ead8eb82bd8e6c061936b4a196d4edf66b
Mohamed Elhanafy <amohamed8684@gmail.com>
2025-06-14 17:30:54 +0300

    update TaskCard component to use theme colors for background and icon tint

--- app/src/main/java/com/example/tudeeapp/presentation/common/extentions/MultipreviewsAnnotations.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/common/extentions/MultipreviewsAnnotations.kt
@@ -4,30 +4,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 
 @Preview(
-    name = "phone",
+    name = "Phone",
     group = "devices",
     device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480"
 )
 @Preview(
-    name = "arabic",
+    name = "Arabic",
     locale = "ar",
     group = "devices",
     device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480"
 )
+
 @Preview(
-    name = "English",
-    locale = "en",
-    group = "devices",
-    device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480"
-)
-@Preview(
-    name = "foldable",
+    name = "Foldable",
     group = "devices",
     device = "spec:shape=Normal,width=673,height=841,unit=dp,dpi=480"
 )
 
 @Preview(
-    name = "tablet",
+    name = "Tablet",
     group = "devices",
     device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480"
 )
