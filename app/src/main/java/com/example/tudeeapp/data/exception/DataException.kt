fb5d98af08ab5a3d1cfca20eed26100b4020e81f
a7med naser <ad0112727@gmail.com>
2025-06-16 17:30:41 +0300

    update the handling of exception in data

--- app/src/main/java/com/example/tudeeapp/data/exception/DataException.kt
+++ app/src/main/java/com/example/tudeeapp/data/exception/DataException.kt
@@ -1,8 +1,11 @@
 package com.example.tudeeapp.data.exception
 
+import com.example.tudeeapp.domain.exception.TudeeException
 
-abstract class DataException(message: String) : Exception(message)
 
-class MappingException( mappingError: String) : DataException("Mapping error : $mappingError")
+abstract class DataException(message: String) : TudeeException(message)
+abstract class MappingException(message: String) : DataException(message)
+class MappingPriorityException(mappingError: String) : DataException("Mapping Priority error : $mappingError")
+class MappingStatusException(mappingError: String) : DataException("Mapping Status error : $mappingError")
 class MappingDateTimeException(mappingError: String) : DataException("Mapping DataTime Error : $mappingError")
 class MappingDrawableException(mappingError: String) : DataException("Mapping From Image Url To Image Drawable Error : $mappingError")
--- app/src/main/java/com/example/tudeeapp/data/mapper/TasksMapper.kt
+++ app/src/main/java/com/example/tudeeapp/data/mapper/TasksMapper.kt
@@ -2,7 +2,8 @@ package com.example.tudeeapp.data.mapper
 
 
 import com.example.tudeeapp.data.exception.MappingDateTimeException
-import com.example.tudeeapp.data.exception.MappingException
+import com.example.tudeeapp.data.exception.MappingPriorityException
+import com.example.tudeeapp.data.exception.MappingStatusException
 import com.example.tudeeapp.data.source.local.room.entity.CategoryEntity
 import com.example.tudeeapp.data.source.local.room.entity.TaskEntity
 import com.example.tudeeapp.domain.models.Category
@@ -16,14 +17,14 @@ fun String.toTaskPriority(): TaskPriority = when (this.uppercase()) {
     "LOW" -> TaskPriority.LOW
     "MEDIUM" -> TaskPriority.MEDIUM
     "HIGH" -> TaskPriority.HIGH
-    else -> throw MappingException(this.uppercase())
+    else -> throw MappingPriorityException(this.uppercase())
 }
 
 fun String.toTaskStatus(): TaskStatus = when (this.uppercase()) {
     "TO_DO" -> TaskStatus.TO_DO
     "IN_PROGRESS" -> TaskStatus.IN_PROGRESS
     "DONE" -> TaskStatus.DONE
-    else -> throw MappingException(this.uppercase())
+    else -> throw MappingStatusException(this.uppercase())
 }
 
 fun TaskEntity.toTask(): Task {
