e8adcbb545cffc0efc6cd5543c8cc663cb1bddb1
a7med naser <ad0112727@gmail.com>
2025-06-16 17:32:04 +0300

    update mapping exception

--- app/src/main/java/com/example/tudeeapp/data/exception/DataException.kt
+++ app/src/main/java/com/example/tudeeapp/data/exception/DataException.kt
@@ -5,7 +5,7 @@ import com.example.tudeeapp.domain.exception.TudeeException
 
 abstract class DataException(message: String) : TudeeException(message)
 abstract class MappingException(message: String) : DataException(message)
-class MappingPriorityException(mappingError: String) : DataException("Mapping Priority error : $mappingError")
-class MappingStatusException(mappingError: String) : DataException("Mapping Status error : $mappingError")
-class MappingDateTimeException(mappingError: String) : DataException("Mapping DataTime Error : $mappingError")
-class MappingDrawableException(mappingError: String) : DataException("Mapping From Image Url To Image Drawable Error : $mappingError")
+class MappingPriorityException(mappingError: String) : MappingException("Mapping Priority error : $mappingError")
+class MappingStatusException(mappingError: String) : MappingException("Mapping Status error : $mappingError")
+class MappingDateTimeException(mappingError: String) : MappingException("Mapping DataTime Error : $mappingError")
+class MappingDrawableException(mappingError: String) : MappingException("Mapping From Image Url To Image Drawable Error : $mappingError")
