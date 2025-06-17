f623e446485c93ba97cceddbb4f8b04733984198
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-06-17 22:25:19 +0300

    refactor: Integrate SnackBar state management into TudeeNavGraph and simplify TudeeScaffold

--- app/src/main/java/com/example/tudeeapp/presentation/navigation/TudeeNavGraph.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/navigation/TudeeNavGraph.kt
@@ -1,13 +1,26 @@
 package com.example.tudeeapp.presentation.navigation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.tudeeapp.presentation.common.components.SnackBar
+import com.example.tudeeapp.presentation.common.components.SnackBarState
 import com.example.tudeeapp.presentation.common.components.TudeeNavigationBar
 import com.example.tudeeapp.presentation.common.components.TudeeScaffold
 import com.example.tudeeapp.presentation.design_system.theme.Theme
@@ -20,75 +33,106 @@ import com.example.tudeeapp.presentation.screen.splash.SplashScreen
 import com.example.tudeeapp.presentation.screen.task.TaskScreen
 import com.example.tudeeapp.presentation.screen.taskDetails.TaskDetailsScreen
 import com.example.tudeeapp.presentation.screen.taskForm.TaskFormScreen
+import kotlinx.coroutines.delay
 
 val LocalNavController = compositionLocalOf<NavHostController> { error("No Nav Controller Found") }
+val LocalSnackBarState = compositionLocalOf<SnackBarState> {
+    error("No SnackBarState provided")
+}
 
 @Composable
 fun TudeeNavGraph() {
     val navController = rememberNavController()
     val backStackEntry = navController.currentBackStackEntryAsState().value
     val currentRoute = backStackEntry?.destination
+    val snackBarState = remember { SnackBarState() }
 
-    CompositionLocalProvider(LocalNavController provides navController) {
+    CompositionLocalProvider(
+        LocalNavController provides navController,
+        LocalSnackBarState provides snackBarState
+    ) {
 
-        TudeeScaffold(
-            bottomBar = {
-                if ((currentRoute?.route != null) && listOf(
-                        Screens.Home::class.qualifiedName,
-                        Screens.Task::class.qualifiedName,
-                        Screens.Category::class.qualifiedName
-                    ).contains(currentRoute.route)
-                )
-                    TudeeNavigationBar(
-                        onItemClick = { navItem ->
-                            navController.navigate(navItem.screen)
-                        }
+        Box {
+            TudeeScaffold(
+                bottomBar = {
+                    if ((currentRoute?.route != null) && listOf(
+                            Screens.Home::class.qualifiedName,
+                            Screens.Task::class.qualifiedName,
+                            Screens.Category::class.qualifiedName
+                        ).contains(currentRoute.route)
                     )
-            },
-            contentBackground = Theme.colors.surfaceColors.surface
-        ) {
-            NavHost(
-                navController = navController,
-                startDestination = Screens.Splash,
+                        TudeeNavigationBar(
+                            onItemClick = { navItem ->
+                                navController.navigate(navItem.screen)
+                            }
+                        )
+                },
+                contentBackground = Theme.colors.surfaceColors.surface
             ) {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screens.Splash,
+                ) {
 
-                composable<Screens.Splash> {
-                    SplashScreen()
+                    composable<Screens.Splash> {
+                        SplashScreen()
+                    }
+
+                    composable<Screens.OnBoarding> {
+                        OnBoardScreen()
+                    }
+
+                    composable<Screens.Home> {
+                        HomeScreen()
+                    }
+
+                    composable<Screens.Task> {
+                        TaskScreen()
+                    }
+
+                    composable<Screens.Category> {
+                        CategoryScreen()
+                    }
+
+
+                    composable<Screens.TaskForm> {
+                        TaskFormScreen()
+                    }
+
+                    composable<Screens.TaskDetails> {
+                        TaskDetailsScreen()
+                    }
+
+                    composable<Screens.CategoriesForm> {
+                        CategoryFormScreen()
+                    }
+
+                    composable<Screens.CategoryDetails> {
+                        CategoryDetailsScreen()
+                    }
+
                 }
+            }
 
-                composable<Screens.OnBoarding> {
-                    OnBoardScreen()
+            if (snackBarState.isVisible == true) {
+                AnimatedVisibility(
+                    visible = snackBarState.isVisible,
+                    enter = fadeIn(animationSpec = tween(snackBarState.durationMillis)),
+                    exit = fadeOut(animationSpec = tween(snackBarState.durationMillis))
+                ) {
+                    SnackBar(
+                        Modifier
+                            .statusBarsPadding()
+                            .padding(start = 16.dp, end = 16.dp, top = 16.dp),
+                        text = snackBarState.message,
+                        isSuccess = snackBarState.isSuccess,
+                        onClick = { snackBarState.hide() }
+                    )
                 }
-
-                composable<Screens.Home> {
-                    HomeScreen()
+                LaunchedEffect(Unit) {
+                    delay(snackBarState.durationMillis.toLong())
+                    snackBarState.hide()
                 }
-
-                composable<Screens.Task> {
-                    TaskScreen()
-                }
-
-                composable<Screens.Category> {
-                    CategoryScreen()
-                }
-
-
-                composable<Screens.TaskForm> {
-                    TaskFormScreen()
-                }
-
-                composable<Screens.TaskDetails> {
-                    TaskDetailsScreen()
-                }
-
-                composable<Screens.CategoriesForm> {
-                    CategoryFormScreen()
-                }
-
-                composable<Screens.CategoryDetails> {
-                    CategoryDetailsScreen()
-                }
-
             }
         }
     }
