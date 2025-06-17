09d1dadd83947aca4617dce1d6d5a84f47df6326
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-06-18 01:56:35 +0300

    implement viewmodel and ui state for task details screen

--- app/src/main/java/com/example/tudeeapp/presentation/screen/taskDetails/TaskDetailsScreen.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/taskDetails/TaskDetailsScreen.kt
@@ -6,16 +6,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,98 +26,50 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tudeeapp.R
+import com.example.tudeeapp.data.mapper.DataConstant.toResDrawables
+import com.example.tudeeapp.domain.models.TaskPriority
+import com.example.tudeeapp.domain.models.TaskStatus
 import com.example.tudeeapp.presentation.common.components.ButtonVariant
 import com.example.tudeeapp.presentation.common.components.PriorityButton
 import com.example.tudeeapp.presentation.common.components.TudeeBottomSheet
 import com.example.tudeeapp.presentation.common.components.TudeeButton
 import com.example.tudeeapp.presentation.design_system.theme.Theme
-
+import com.example.tudeeapp.presentation.screen.taskDetails.state.CategoryUiState
+import com.example.tudeeapp.presentation.screen.taskDetails.state.TaskUiState
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun TaskDetailsScreen() {
+fun TaskDetailsScreen(
+    viewModel: TaskDetailsViewModel = koinViewModel()
+) {
 
+    val taskDetailsUiState by viewModel.uiState.collectAsState()
     var isSheetOpen by remember { mutableStateOf(true) }
+
     TudeeBottomSheet(
         isVisible = isSheetOpen,
-        title = "Task details",
+        title = stringResource(R.string.task_details),
         isScrollable = false,
         skipPartiallyExpanded = false,
         onDismiss = { isSheetOpen = true },
         content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Theme.colors.surfaceColors.surface)
-                    .padding(horizontal = 16.dp),
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(56.dp)
-                        .padding(top = 12.dp)
-                        .background(
-                            color = Theme.colors.surfaceColors.surfaceHigh,
-                            shape = CircleShape
-                        )
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_education),
-                        contentDescription = "task category icon",
-                        tint = Color.Unspecified,
-                        modifier = Modifier
-                            .size(32.dp)
-                            .align(Alignment.Center)
-                    )
-                }
 
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = "Organize Study Desk",
-                    style = Theme.textStyle.title.medium,
-                    color = Theme.colors.text.title
-                )
-
-                Text(
-                    modifier = Modifier.padding(top = 8.dp),
-                    text = "Solve all exercises from page 45 to 50 in the textbook, Solve all exercises from page 45 to 50 in the textbook.",
-                    style = Theme.textStyle.body.medium,
-                    color = Theme.colors.text.body
-                )
-                Spacer(
-                    modifier = Modifier
-                        .padding(vertical = 12.dp)
-                        .background(Theme.colors.stroke)
-                )
-                Row {
-                    BoxTaskStatus()
-                    PriorityButton(
-                        modifier = Modifier
-                            .padding(start = 8.dp),
-                        text = "High",
-                        icon = painterResource(id = R.drawable.ic_flag),
-                        backgroundColor = Theme.colors.status.pinkAccent,
-                        onClick = {}
-                    )
-                }
-                Row(modifier = Modifier.padding(top = 24.dp, bottom = 24.dp)) {
-                    TudeeButton(
-                        onClick = { },
-                        icon = {
-                            Icon(
-                                painter = painterResource(R.drawable.ic_pencil_edit),
-                                null,
-                                modifier = Modifier.size(24.dp)
-                            )
-                        },
-                        variant = ButtonVariant.OutlinedButton,
-                    )
-                    Spacer(Modifier.width(4.dp))
-                    TudeeButton(
-                        modifier = Modifier.weight(1f),
-                        onClick = { }, text = "Move to done",
-                        variant = ButtonVariant.OutlinedButton,
+            if (taskDetailsUiState.isLoading) {
+                LoadingView()
+            } else if (taskDetailsUiState.errorMessage.isNotEmpty()) {
+                ErrorView(message = taskDetailsUiState.errorMessage)
+            } else {
+                val taskUiState = taskDetailsUiState.taskUiState
+                val categoryUiState = taskDetailsUiState.categoryUiState
+                if (taskUiState != null && categoryUiState != null) {
+                    TaskDetailsContent(
+                        taskUiState = taskUiState,
+                        categoryUiState = categoryUiState,
+                        onStatusChange = { newStatus -> viewModel.updateTaskStatus(newStatus) }
                     )
                 }
             }
@@ -125,11 +78,146 @@ fun TaskDetailsScreen() {
 }
 
 @Composable
-private fun BoxTaskStatus() {
+fun LoadingView() {
+    CircularProgressIndicator()
+}
+
+@Composable
+fun TaskDetailsContent(
+    taskUiState: TaskUiState,
+    categoryUiState: CategoryUiState,
+    onStatusChange: (TaskStatus) -> Unit
+) {
+    //TODO Ask If category icon only from drawable
+    //val painter = rememberAsyncImagePainter(categoryUiState.imageUrl)
+    val iconPriority = when (taskUiState.priority) {
+        TaskPriority.LOW -> painterResource(id = R.drawable.ic_trade_down)
+        TaskPriority.MEDIUM -> painterResource(id = R.drawable.ic_alert)
+        TaskPriority.HIGH -> painterResource(id = R.drawable.ic_flag)
+    }
+
+    val backgroundPriorityColor = when (taskUiState.priority) {
+        TaskPriority.LOW -> Theme.colors.status.greenAccent
+        TaskPriority.MEDIUM -> Theme.colors.status.yellowAccent
+        TaskPriority.HIGH -> Theme.colors.status.pinkAccent
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Theme.colors.surfaceColors.surface)
+            .padding()
+            .padding(start = 16.dp, end = 16.dp, bottom = 24.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .size(56.dp)
+                .padding(top = 12.dp)
+                .background(
+                    color = Theme.colors.surfaceColors.surfaceHigh,
+                    shape = CircleShape
+                )
+        ) {
+            Icon(
+                painter = painterResource(categoryUiState.imageUrl.toResDrawables()),
+                contentDescription = "task category icon",
+                tint = Color.Unspecified,
+                modifier = Modifier
+                    .size(32.dp)
+                    .align(Alignment.Center)
+            )
+        }
+
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = taskUiState.title,
+            style = Theme.textStyle.title.medium,
+            color = Theme.colors.text.title
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            text = taskUiState.description,
+            style = Theme.textStyle.body.medium,
+            color = Theme.colors.text.body
+        )
+        Spacer(
+            modifier = Modifier
+                .padding(vertical = 12.dp)
+                .height(1.dp)
+                .background(Theme.colors.stroke)
+        )
+        Row {
+            BoxTaskStatus(taskUiState)
+            PriorityButton(
+                modifier = Modifier
+                    .padding(start = 8.dp),
+                text = taskUiState.priority.name.lowercase().replaceFirstChar { it.uppercase() },
+                icon = iconPriority,
+                backgroundColor = backgroundPriorityColor,
+                onClick = {}
+            )
+        }
+        if (taskUiState.status != TaskStatus.DONE)
+            Row(modifier = Modifier.padding(top = 24.dp)) {
+                TudeeButton(
+                    onClick = {
+                        val newStatus = if (taskUiState.status == TaskStatus.IN_PROGRESS) {
+                            TaskStatus.DONE
+                        } else {
+                            TaskStatus.IN_PROGRESS
+                        }
+                        onStatusChange(newStatus)
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_pencil_edit),
+                            null,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    },
+                    variant = ButtonVariant.OutlinedButton,
+                )
+                Spacer(Modifier.width(4.dp))
+                TudeeButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = { },
+                    text = if (taskUiState.status == TaskStatus.IN_PROGRESS) {
+                        stringResource(R.string.move_to_done)
+                    } else {
+                        stringResource(R.string.move_to_in_progress)
+                    },
+
+                    variant = ButtonVariant.OutlinedButton,
+                )
+            }
+    }
+}
+
+@Composable
+private fun ErrorView(message: String) {
+    //TODO add snack bar component
+}
+
+@Composable
+private fun BoxTaskStatus(taskUiState: TaskUiState) {
+
+    val textStatusColor = when (taskUiState.status) {
+        TaskStatus.DONE -> Theme.colors.status.yellowAccent
+        TaskStatus.TO_DO -> Theme.colors.status.greenAccent
+        TaskStatus.IN_PROGRESS -> Theme.colors.status.purpleVariant
+    }
+
+    val backgroundStatusColor = when (taskUiState.status) {
+        TaskStatus.DONE -> Theme.colors.status.yellowVariant
+        TaskStatus.TO_DO -> Theme.colors.status.greenVariant
+        TaskStatus.IN_PROGRESS -> Theme.colors.status.purpleVariant
+    }
+
     Box(
         modifier = Modifier
             .clip(RoundedCornerShape(100.dp))
-            .background(Theme.colors.status.purpleVariant)
+            .background(backgroundStatusColor)
             .padding(horizontal = 12.dp, vertical = 6.dp)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
@@ -137,13 +225,13 @@ private fun BoxTaskStatus() {
                 modifier = Modifier
                     .clip(CircleShape)
                     .size(5.dp)
-                    .background(Theme.colors.status.purpleAccent)
+                    .background(textStatusColor)
             )
             Text(
                 modifier = Modifier.padding(start = 4.dp),
-                text = "in progress",
+                text = taskUiState.status.name.lowercase().replaceFirstChar { it.uppercase() },
                 style = Theme.textStyle.label.small,
-                color = Theme.colors.status.purpleAccent
+                color = textStatusColor
             )
         }
     }
@@ -153,5 +241,5 @@ private fun BoxTaskStatus() {
 @Preview
 @Composable
 private fun TaskDetailsScreenPreview() {
-    TaskDetailsScreen()
+    // TaskDetailsScreen(123123)
 }
