62556fa5e7c72c555c4e3474509083a58daa0010
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-06-18 01:48:05 +0300

    add ui state and view model for task details

---
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/taskDetails/TaskDetailsViewModel.kt
@@ -0,0 +1,81 @@
+package com.example.tudeeapp.presentation.screen.taskDetails
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import com.example.tudeeapp.domain.TaskServices
+import com.example.tudeeapp.domain.exception.CategoryException
+import com.example.tudeeapp.domain.exception.TaskException
+import com.example.tudeeapp.domain.models.TaskStatus
+import com.example.tudeeapp.presentation.navigation.Screens
+import com.example.tudeeapp.presentation.screen.taskDetails.state.TaskDetailsUiState
+import com.example.tudeeapp.presentation.screen.taskDetails.state.toCategoryUiState
+import com.example.tudeeapp.presentation.screen.taskDetails.state.toTaskUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class TaskDetailsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val taskServices: TaskServices
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(TaskDetailsUiState())
+    val uiState: StateFlow<TaskDetailsUiState> = _uiState.asStateFlow()
+
+    val taskId = savedStateHandle.toRoute<Screens.TaskDetails>().taskId
+
+    init {
+        loadTaskDetails()
+    }
+
+    private fun loadTaskDetails() {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isLoading = true)
+            try {
+                taskServices.getTaskById(taskId).collect { task ->
+                    taskServices.getCategoryById(task.categoryId).collect { category ->
+                        _uiState.value = _uiState.value.copy(
+                            taskUiState = task.toTaskUiState(),
+                            categoryUiState = category.toCategoryUiState()
+                        )
+                    }
+                }
+            } catch (e: TaskException) {
+                _uiState.value = _uiState.value.copy(
+                    isLoading = false,
+                    errorMessage = e.message.toString()
+                )
+            } catch (e: CategoryException) {
+                _uiState.value = _uiState.value.copy(
+                    isLoading = false,
+                    errorMessage = e.message.toString()
+                )
+            }
+
+        }
+    }
+
+    fun updateTaskStatus(newStatus: TaskStatus) {
+        val currentTaskUiState = _uiState.value.taskUiState
+        if (currentTaskUiState != null) {
+            viewModelScope.launch {
+                try {
+
+                    taskServices.updateTaskStatus(currentTaskUiState.id, newStatus)
+
+                    val updatedTask = currentTaskUiState.copy(status = newStatus)
+
+                    _uiState.value = _uiState.value.copy(taskUiState = updatedTask)
+
+                } catch (e: TaskException) {
+                    _uiState.value =
+                        _uiState.value.copy(errorMessage = e.message ?: "Unknown error")
+                }
+            }
+        }
+    }
+
+}
