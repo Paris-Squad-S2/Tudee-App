a5da3ad57fe87c2aaf306e372b03ec6e38a19799
Asmaa Karam <asmaakaram793@gmai.com>
2025-06-17 17:52:05 +0300

    update DayItem to be Clickable

--- app/src/main/java/com/example/tudeeapp/presentation/common/components/DayItem.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/common/components/DayItem.kt
@@ -1,6 +1,7 @@
 package com.example.tudeeapp.presentation.common.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,7 +21,13 @@ import com.example.tudeeapp.presentation.common.extentions.PreviewMultiDevices
 import com.example.tudeeapp.presentation.design_system.theme.Theme
 
 @Composable
-fun DayItem(isSelected: Boolean, dayNumber: String, dayName: String, modifier: Modifier = Modifier) {
+fun DayItem(
+    isSelected: Boolean,
+    dayNumber: String,
+    dayName: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     val dayNumberTextColor = if (isSelected) Theme.colors.surfaceColors.onPrimaryColors.onPrimary else Theme.colors.text.body
     val dayNameTextColor = if (isSelected) Theme.colors.surfaceColors.onPrimaryColors.onPrimaryCaption else Theme.colors.text.hint
     val backgroundColor = if (isSelected) Brush.verticalGradient(Theme.colors.primaryGradient.colors)
@@ -35,7 +42,7 @@ fun DayItem(isSelected: Boolean, dayNumber: String, dayName: String, modifier: M
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(horizontal = 16.dp , vertical = 12.dp)
+            modifier = Modifier.padding(horizontal = 16.dp , vertical = 12.dp).clickable { onClick() }
         ) {
             Text(
                 text = dayNumber,
@@ -57,8 +64,8 @@ fun DayItem(isSelected: Boolean, dayNumber: String, dayName: String, modifier: M
 private fun DayItemPreview() {
     BasePreview{
         Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-            DayItem(isSelected = true, dayNumber = "12", dayName = "Mon")
-            DayItem(isSelected = false, dayNumber = "12", dayName = "Mon")
+            DayItem(isSelected = true, dayNumber = "12", dayName = "Mon", {})
+            DayItem(isSelected = false, dayNumber = "12", dayName = "Mon", {})
         }
     }
 }
