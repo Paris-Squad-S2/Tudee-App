c84c30e1ef4fd419376042ca85b247b6f5e6c478
IslamMagd <eslam752001.magdy54@gmail.com>
2025-06-18 00:32:12 +0300

    move CategoryUIState mapper in CategoryUiState.kt

--- app/src/main/java/com/example/tudeeapp/presentation/screen/categories/CategoryUiState.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/categories/CategoryUiState.kt
@@ -1,6 +1,8 @@
 package com.example.tudeeapp.presentation.screen.categories
 
 import androidx.annotation.DrawableRes
+import com.example.tudeeapp.data.mapper.DataConstant.toResDrawables
+import com.example.tudeeapp.domain.models.Category
 
 data class CategoryUIState(
     val categories: List<CategoryItemUIState> = emptyList(),
@@ -14,3 +16,12 @@ data class CategoryItemUIState(
     @DrawableRes val imageResId: Int,
     val count: Int
 )
+
+fun Category.toCategoryUIState(): CategoryItemUIState {
+    return CategoryItemUIState(
+        name = this.title,
+        imageResId = this.imageUrl.toResDrawables(),
+        count = this.tasksCount,
+        id = this.id
+    )
+}
