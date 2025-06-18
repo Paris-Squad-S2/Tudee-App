8b6657ea8fbb089ed5081c6b85f16409018a1a76
a7med naser <ad0112727@gmail.com>
2025-06-18 03:29:02 +0300

    add taskUi  and his mapper and handle home ui state

--- app/src/main/java/com/example/tudeeapp/presentation/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/home/HomeViewModel.kt
@@ -2,9 +2,11 @@ package com.example.tudeeapp.presentation.screen.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.tudeeapp.R
 import com.example.tudeeapp.domain.TaskServices
 import com.example.tudeeapp.domain.exception.TudeeException
 import com.example.tudeeapp.domain.models.Task
+import com.example.tudeeapp.domain.models.TaskPriority
 import com.example.tudeeapp.domain.models.TaskStatus
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,7 +18,7 @@ import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val taskServices: TaskServices
-): ViewModel() {
+) : ViewModel() {
 
     private val _homeState = MutableStateFlow(HomeViewModeUiState())
     val homeState = _homeState
@@ -27,20 +29,25 @@ class HomeViewModel(
             initialValue = HomeViewModeUiState()
         )
 
+
     fun getTasks() {
         viewModelScope.launch(IO) {
             try {
+                _homeState.update { it.copy(isLoading = true) }
                 taskServices.getAllTasks().collect { tasks ->
-                    if (tasks.isEmpty()) {
+                    val tasksUi = tasks.map { it.toTaskUi() }
+                    if (tasksUi.isEmpty()) {
                         _homeState.update {
-                            it.copy(isTasksEmpty = true)
+                            it.copy(isTasksEmpty = true, isLoading = false, isSuccess = true)
                         }
-                    }else {
+                    } else {
                         _homeState.update {
                             it.copy(
-                                inProgressTasks = tasks.filter { it.status == TaskStatus.IN_PROGRESS },
-                                toDoTasks = tasks.filter { it.status == TaskStatus.TO_DO },
-                                doneTasks = tasks.filter { it.status == TaskStatus.DONE },
+                                inProgressTasks = tasksUi.filter { it.status == TaskStatus.IN_PROGRESS },
+                                toDoTasks = tasksUi.filter { it.status == TaskStatus.TO_DO },
+                                doneTasks = tasksUi.filter { it.status == TaskStatus.DONE },
+                                isSuccess = true,
+                                isLoading = false,
                                 isTasksEmpty = false
                             )
                         }
@@ -49,7 +56,7 @@ class HomeViewModel(
 
             } catch (e: TudeeException) {
                 _homeState.update {
-                    it.copy(error = e.message)
+                    it.copy(error = e.message, isLoading = false)
                 }
             }
         }
@@ -60,9 +67,35 @@ class HomeViewModel(
 }
 
 data class HomeViewModeUiState(
-    val inProgressTasks: List<Task> = emptyList(),
-    val toDoTasks: List<Task> = emptyList(),
-    val doneTasks: List<Task> = emptyList(),
+    val inProgressTasks: List<TaskUi> = emptyList(),
+    val toDoTasks: List<TaskUi> = emptyList(),
+    val doneTasks: List<TaskUi> = emptyList(),
     val isTasksEmpty: Boolean = true,
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false,
     val error: String? = null
 )
+
+data class TaskUi(
+    val id: Long = 0L,
+    val title: String,
+    val description: String,
+    val priority: TaskPriority,
+    val status: TaskStatus,
+    val resIconId: Int
+)
+
+fun Task.toTaskUi(): TaskUi {
+    return TaskUi(
+        id = this.id,
+        title = title,
+        description = description,
+        priority = priority,
+        status = this.status,
+        resIconId = when (priority) {
+            TaskPriority.LOW -> R.drawable.ic_trade_down
+            TaskPriority.MEDIUM -> android.R.drawable.ic_dialog_alert
+            TaskPriority.HIGH -> R.drawable.ic_flag
+        }
+    )
+}
