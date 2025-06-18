099599464b97076c831393a79ad071550f2f39e8
a7med naser <ad0112727@gmail.com>
2025-06-18 06:13:19 +0300

    add no tasks found exception and no categories found exception

--- app/src/main/java/com/example/tudeeapp/domain/exception/TudeeException.kt
+++ app/src/main/java/com/example/tudeeapp/domain/exception/TudeeException.kt
@@ -1,5 +1,8 @@
 package com.example.tudeeapp.domain.exception
 
 abstract class TudeeException(message: String) : Exception(message)
-class TaskException(message: String="") : TudeeException("Task exception : $message")
-class CategoryException(message: String="") : TudeeException("Category exception : $message")
+open class TaskException(message: String) : TudeeException(message)
+open class CategoryException(message: String="") : TudeeException(message)
+// customize your Exception
+class NoTasksFoundException() : TaskException("No Tasks Found Exception ")
+class NoCategoriesFoundException() : CategoryException("No Categories Found Exception")
