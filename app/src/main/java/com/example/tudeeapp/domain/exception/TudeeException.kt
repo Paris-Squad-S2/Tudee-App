5ced8af2051f894c0d90bf88fdfd0233f8f49dd5
a7med naser <ad0112727@gmail.com>
2025-06-18 05:39:06 +0300

    update tudee exception

--- app/src/main/java/com/example/tudeeapp/domain/exception/TudeeException.kt
+++ app/src/main/java/com/example/tudeeapp/domain/exception/TudeeException.kt
@@ -1,5 +1,5 @@
 package com.example.tudeeapp.domain.exception
 
-abstract class TudeeException(message: String) : Throwable(message)
+abstract class TudeeException(message: String) : Exception(message)
 class TaskException(message: String="") : TudeeException("Task exception : $message")
 class CategoryException(message: String="") : TudeeException("Category exception : $message")
