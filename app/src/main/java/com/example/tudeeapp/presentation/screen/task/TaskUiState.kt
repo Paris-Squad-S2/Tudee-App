a0ffe8b515be7dba3fd9d3b287f833b5b6103a3a
Asmaa Karam <asmaakaram793@gmai.com>
2025-06-18 16:14:21 +0300

    add mapper function to TaskUiState

--- app/src/main/java/com/example/tudeeapp/presentation/screen/task/TaskUiState.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/task/TaskUiState.kt
@@ -1,24 +1,40 @@
 package com.example.tudeeapp.presentation.screen.task
 
+import com.example.tudeeapp.domain.models.Task
+import com.example.tudeeapp.domain.models.TaskPriority
+import com.example.tudeeapp.domain.models.TaskStatus
+import com.example.tudeeapp.presentation.utills.TaskPriorityUi
 import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
-sealed class TaskUiState{
-    data class success(val data:TasksUi):TaskUiState()
-    object isLoading:TaskUiState()
-    data class error(val message:String):TaskUiState()
-}
+data class TaskUiState(
+    val data: TasksUi = TasksUi(),
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
 
 data class TasksUi(
-    val tasks: List<TaskItemUiState> = emptyList(),
+    val calender: CalenderUi = CalenderUi(),
+    val status: List<StatusUi> = emptyList(),
     val selectedStatus: TaskStatusUi = TaskStatusUi.TO_DO,
-    val selectedDate: LocalDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date,
-    val daysOfMonth: List<LocalDate> = emptyList(),
-    val currentMonthYear: String = "",
-    val todayIndex: Int = 0,
+    val tasks: List<TaskItemUiState> = emptyList(),
     val showDatePicker: Boolean = false,
+    val todayIndex:Int = 0
+)
+
+data class CalenderUi(
+    val currentMonthYear: String = "",
+    val daysOfMonth: List<DayUi> = emptyList(),
+    val selectedDate: LocalDate = Clock.System.now()
+        .toLocalDateTime(TimeZone.currentSystemDefault()).date
+)
+
+data class DayUi(
+    val num: Int,
+    val name: String,
+    val date: LocalDate
 )
 
 data class TaskItemUiState(
@@ -31,14 +47,53 @@ data class TaskItemUiState(
     val categoryId: Long
 )
 
-enum class TaskPriorityUi {
-    HIGH,
-    MEDIUM,
-    LOW
+data class StatusUi(
+    val name:String = "",
+    val count:Int = 0,
+    val isSelected:Boolean = false
+)
+
+enum class TaskStatusUi(val status: String) {
+    IN_PROGRESS("In progress"),
+    TO_DO("TO DO"),
+    DONE("Done")
 }
 
-enum class TaskStatusUi {
-    TO_DO,
-    IN_PROGRESS,
-    DONE
+fun List<Task>.toTasksUi(): List<TaskItemUiState> {
+    return this.map { it.toTaskUiState() }
+}
+
+fun Task.toTaskUiState(): TaskItemUiState {
+    return TaskItemUiState(
+        id = this.id,
+        title = this.title,
+        description = this.description,
+        priority = this.priority.toUiPriority(),
+        status = this.status.toUiStatus(),
+        createdDate = this.createdDate,
+        categoryId = this.categoryId
+    )
+}
+
+fun TaskPriority.toUiPriority(): TaskPriorityUi = when (this) {
+    TaskPriority.HIGH -> TaskPriorityUi.HIGH
+    TaskPriority.MEDIUM -> TaskPriorityUi.MEDIUM
+    TaskPriority.LOW -> TaskPriorityUi.LOW
+}
+
+fun TaskStatus.toUiStatus(): TaskStatusUi = when (this) {
+    TaskStatus.TO_DO -> TaskStatusUi.TO_DO
+    TaskStatus.IN_PROGRESS -> TaskStatusUi.IN_PROGRESS
+    TaskStatus.DONE -> TaskStatusUi.DONE
+}
+
+fun LocalDate.createCalendarUi(
+    currentMonthYear:String,
+    daysOfMonth:List<DayUi>,
+): CalenderUi {
+    return CalenderUi(
+        currentMonthYear = currentMonthYear,
+        daysOfMonth = daysOfMonth,
+        selectedDate = this
+    )
 }
