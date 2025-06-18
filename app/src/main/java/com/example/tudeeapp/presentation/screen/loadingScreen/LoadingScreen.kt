ec1a72fad8d3011a1477ca556252231a488c7463
=Muhammed Wael <=mw26200400@gmail.com>
2025-06-18 17:34:55 +0300

    add error and loading screens

---
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/loadingScreen/LoadingScreen.kt
@@ -0,0 +1,21 @@
+package com.example.tudeeapp.presentation.screen.loadingScreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        androidx.compose.material3.CircularProgressIndicator()
+    }
+}
