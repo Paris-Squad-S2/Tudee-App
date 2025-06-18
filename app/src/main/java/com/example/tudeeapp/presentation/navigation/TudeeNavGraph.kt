c5ebf4f2e33a2537498733c486d3ace7a2e592ce
Yousef Osama Kamal <143261536+yousef-osama11@users.noreply.github.com>
2025-06-18 08:54:31 +0300

    Bugfix/design_system_issues (#13)
    * update CategoryItem to be clickable
    
    * update DayItem to be clickable
    
    * update HorizontalTabs to with fixed height
    
    * update tint color od PriorityButton
    
    * update clickable of TaskItem to be in the item not on button
    
    * update TopAppBar icon
    
    * update TudeeNavigationBar background
    
    * update TextField Focused
    
    * update TudeeDatePickerDialog dismiss function
    
    * update TudeeBottomSheet to be scrollable
    
    * add TextTopBar
    
    * update CategoriesScreen name
    
    * update EmptyTasksSection component
    
    * add TaskPriorityUi mapper
    
    * replace text with row
    
    ---------
    
    Co-authored-by: Asmaa Karam <asmaakaram793@gmai.com>

--- app/src/main/java/com/example/tudeeapp/presentation/navigation/TudeeNavGraph.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/navigation/TudeeNavGraph.kt
@@ -12,7 +12,7 @@ import com.example.tudeeapp.presentation.common.components.TudeeNavigationBar
 import com.example.tudeeapp.presentation.common.components.TudeeScaffold
 import com.example.tudeeapp.presentation.design_system.theme.Theme
 import com.example.tudeeapp.presentation.screen.categoriesForm.CategoryFormScreen
-import com.example.tudeeapp.presentation.screen.category.CategoryScreen
+import com.example.tudeeapp.presentation.screen.category.CategoriesScreen
 import com.example.tudeeapp.presentation.screen.categoryDetails.CategoryDetailsScreen
 import com.example.tudeeapp.presentation.screen.home.HomeScreen
 import com.example.tudeeapp.presentation.screen.onBoarding.OnBoardScreen
@@ -69,7 +69,7 @@ fun TudeeNavGraph() {
                 }
 
                 composable<Screens.Category> {
-                    CategoryScreen()
+                    CategoriesScreen()
                 }
 
 
