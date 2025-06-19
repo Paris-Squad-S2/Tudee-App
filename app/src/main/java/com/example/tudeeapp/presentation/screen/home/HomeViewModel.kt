5bbb249c7e5e58a5824eb36be3039edde6cc67da
a7med naser <ad0112727@gmail.com>
2025-06-19 17:10:01 +0300

    handle on toggled action in view model and add handle category icon in tasks card

--- app/src/main/java/com/example/tudeeapp/presentation/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.tudeeapp.presentation.screen.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tudeeapp.R
+import com.example.tudeeapp.data.mapper.DataConstant.toResDrawables
 import com.example.tudeeapp.domain.TaskServices
 import com.example.tudeeapp.domain.exception.TudeeException
 import com.example.tudeeapp.domain.models.Task
@@ -11,6 +12,7 @@ import com.example.tudeeapp.domain.models.TaskStatus
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -20,22 +22,62 @@ class HomeViewModel(
     private val taskServices: TaskServices
 ) : ViewModel() {
 
-    private val _homeState = MutableStateFlow(HomeViewModeUiState())
+    private val _homeState = MutableStateFlow(HomeUiState())
     val homeState = _homeState
-        .onStart { getTasks() }
+        .onStart {
+            getTasksIcons()
+            getTasks()
+        }
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
-            initialValue = HomeViewModeUiState()
+            initialValue = HomeUiState()
         )
 
+
+    fun onToggledAction(isDarkMode: Boolean) {
+        _homeState.update {
+            it.copy(isDarkMode = isDarkMode)
+        }
+    }
+
+    fun getTasksIcons() {
+        viewModelScope.launch(IO) {
+            try {
+                taskServices.getAllTasks().collect { tasks ->
+                    val tasksIcons = tasks.map { taskServices.getCategoryById(it.categoryId).first().imageUrl }
+                                          .map { it.toResDrawables() }
+
+                    val tasksUi = tasks.map { it.toTaskUi() }
+                        .mapIndexed { index, taskUi -> taskUi.copy(categoryIcon = tasksIcons[index]) }
+                    _homeState.update {
+                        it.copy(
+                            inProgressTasks = tasksUi.filter { it.status == TaskStatus.IN_PROGRESS },
+                            toDoTasks = tasksUi.filter { it.status == TaskStatus.TO_DO },
+                            doneTasks = tasksUi.filter { it.status == TaskStatus.DONE },
+                            isSuccess = true,
+                            isLoading = false,
+                            isTasksEmpty = false
+                        )
+                    }
+                }
+            } catch (e: Exception) {
+                _homeState.update {
+                    it.copy(error = e.message, isLoading = false, isSuccess = false)
+                }
+            }
+        }
+    }
 
     fun getTasks() {
         viewModelScope.launch(IO) {
             try {
                 _homeState.update { it.copy(isLoading = true) }
                 taskServices.getAllTasks().collect { tasks ->
-                    val tasksUi = tasks.map { it.toTaskUi() }
+                    val tasksUi = tasks
+                        .map { it.toTaskUi() }
+
+
                     if (tasksUi.isEmpty()) {
                         _homeState.update {
                             it.copy(isTasksEmpty = true, isLoading = false, isSuccess = true)
@@ -56,7 +98,7 @@ class HomeViewModel(
 
             } catch (e: TudeeException) {
                 _homeState.update {
-                    it.copy(error = e.message, isLoading = false)
+                    it.copy(error = e.message, isLoading = false, isSuccess = false)
                 }
             }
         }
@@ -64,38 +106,24 @@ class HomeViewModel(
     }
 
 
+    private fun Task.toTaskUi(): TaskUi {
+        return TaskUi(
+            id = this.id,
+            title = title,
+            description = description,
+            priority = priority,
+            priorityResIcon = when (priority) {
+                TaskPriority.LOW -> R.drawable.ic_trade_down
+                TaskPriority.MEDIUM -> android.R.drawable.ic_dialog_alert
+                TaskPriority.HIGH -> R.drawable.ic_flag
+            },
+            status = this.status,
+        )
+    }
+
 }
 
-data class HomeViewModeUiState(
-    val inProgressTasks: List<TaskUi> = emptyList(),
-    val toDoTasks: List<TaskUi> = emptyList(),
-    val doneTasks: List<TaskUi> = emptyList(),
-    val isTasksEmpty: Boolean = true,
-    val isLoading: Boolean = false,
-    val isSuccess: Boolean = false,
-    val error: String? = null
-)
 
-data class TaskUi(
-    val id: Long = 0L,
-    val title: String,
-    val description: String,
-    val priority: TaskPriority,
-    val status: TaskStatus,
-    val resIconId: Int
-)
 
-fun Task.toTaskUi(): TaskUi {
-    return TaskUi(
-        id = this.id,
-        title = title,
-        description = description,
-        priority = priority,
-        status = this.status,
-        resIconId = when (priority) {
-            TaskPriority.LOW -> R.drawable.ic_trade_down
-            TaskPriority.MEDIUM -> android.R.drawable.ic_dialog_alert
-            TaskPriority.HIGH -> R.drawable.ic_flag
-        }
-    )
-}
+
+
