f7859b8197e217644f802bd8ddd2d3b227cba766
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-06-19 04:06:04 +0300

    resolve conflict from development

--- app/src/main/java/com/example/tudeeapp/di/ViewModelModule.kt
+++ app/src/main/java/com/example/tudeeapp/di/ViewModelModule.kt
@@ -1,6 +1,8 @@
 package com.example.tudeeapp.di
 
 import com.example.tudeeapp.MainViewModel
+import com.example.tudeeapp.presentation.screen.categories.CategoriesViewModel
+import com.example.tudeeapp.presentation.screen.onBoarding.OnboardingViewModel
 import com.example.tudeeapp.presentation.screen.taskDetails.TaskDetailsViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
