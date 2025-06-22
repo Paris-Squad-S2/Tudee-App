03d82e1e945e26d1f8d475b80b31fc3006dc52e0
dina othman <dinaosman581@gmail.com>
2025-06-22 15:56:16 +0300

    refactor onboarding screen

--- app/src/main/java/com/example/tudeeapp/presentation/screen/onBoarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/onBoarding/OnBoardingScreen.kt
@@ -6,30 +6,37 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.tudeeapp.presentation.navigation.LocalNavController
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tudeeapp.R
-import com.example.tudeeapp.presentation.design_system.theme.Theme
-import com.example.tudeeapp.presentation.navigation.Screens
-import com.example.tudeeapp.presentation.screen.onBoarding.components.OnBoardingPage
+import com.example.tudeeapp.presentation.screen.onBoarding.components.OnBoardingImage
 import com.example.tudeeapp.presentation.screen.onBoarding.components.PageIndicator
 import kotlinx.coroutines.launch
 import org.koin.compose.viewmodel.koinViewModel
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tudeeapp.R
+import com.example.tudeeapp.presentation.common.extentions.BasePreview
+import com.example.tudeeapp.presentation.design_system.theme.Theme
+import com.example.tudeeapp.presentation.navigation.Screens
+import com.example.tudeeapp.presentation.screen.onBoarding.components.BottomSection
+
 
 @Composable
 fun OnBoardScreen(
@@ -68,40 +75,57 @@ fun OnBoardingScreenContent(
 ){
 
     val pagerState= rememberPagerState(initialPage = 0) { pages.size }
+    val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(pagerState.currentPage) {
         onPageChanged(pagerState.currentPage)
     }
     Box {
-        Column(modifier = modifier) {
-            val coroutineScope = rememberCoroutineScope()
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(Theme.colors.status.overlay)
+                .statusBarsPadding()
+        ) {
+            if (pagerState.currentPage != pages.lastIndex) {
+                TextButton(
+                    modifier = Modifier.align(Alignment.Start),
+                    onClick = onFinished
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.skip),
+                        color = Theme.colors.primary,
+                        style = Theme.textStyle.label.large
+                    )
+                }
+            }
             HorizontalPager(
                 state = pagerState,
                 modifier = Modifier.weight(1f)
             ) { index ->
-                OnBoardingPage(
-                    page = pages[index],
-                    onNextClick = {
-                        if (pagerState.currentPage == pages.lastIndex) {
-                            onFinished()
-                        } else {
-                            coroutineScope.launch { pagerState.animateScrollToPage(index + 1) }
-                        }
-                    },
-                    onSkipClick = {
-                        onFinished()
-                    },
-                    isLastPage = index == 2
+                OnBoardingImage(
+                    page = pages[index]
                 )
             }
+            BottomSection(
+                page = pages[state.currentPage],
+                onNextClick = {
+                    if (pagerState.currentPage == pages.lastIndex) {
+                        onFinished()
+                    } else {
+                        coroutineScope.launch { pagerState.animateScrollToPage(pagerState.currentPage + 1) }
+                    }
+                }
+            )
+
             Row(
                 modifier = Modifier
-                    .background(Theme.colors.status.overlay)
                     .fillMaxWidth()
                     .padding(bottom = 24.dp),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.Bottom
             ) { PageIndicator(pageSize = pages.size, currentPage = state.currentPage) }
+
         }
         Image(
             painter = painterResource(id = R.drawable.background_vectors),
@@ -113,13 +137,18 @@ fun OnBoardingScreenContent(
 
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+    name = "Galaxy J5 Prime",
+    device = "spec:width=360dp,height=640dp,dpi=320"
+)
 @Composable
 fun OnBoardingScreenPreview(){
-    OnBoardingScreenContent(
-        pages = onboardingPages(),
-        onFinished = {},
-        onPageChanged = {},
-        state = OnboardingState()
-    )
+    BasePreview {
+        OnBoardingScreenContent(
+            pages = onboardingPages(),
+            onFinished = {},
+            onPageChanged = {},
+            state = OnboardingState()
+        )
+    }
 }
