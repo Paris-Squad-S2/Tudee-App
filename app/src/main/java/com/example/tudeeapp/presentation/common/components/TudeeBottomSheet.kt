2dc2cd84430d62e4977a5e2e5e9e24be60516d17
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-06-26 02:16:34 +0300

    refactor: add status bar padding to bottom sheet layout

--- app/src/main/java/com/example/tudeeapp/presentation/common/components/TudeeBottomSheet.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/common/components/TudeeBottomSheet.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -73,7 +74,8 @@ fun TudeeBottomSheet(
     BoxWithConstraints {
         ModalBottomSheet(
             modifier = modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .statusBarsPadding(),
             containerColor = Theme.colors.surfaceColors.surface,
             onDismissRequest = { },
             dragHandle = {
