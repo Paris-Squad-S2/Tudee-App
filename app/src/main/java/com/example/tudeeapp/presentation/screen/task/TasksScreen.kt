fae9a3a66151de6b8ad411b609311c8c7cd988b6
MuhammedWael9991 <mw26200400@gmail.com>
2025-06-27 01:06:58 +0300

    adjust TasksScreen layout for landscape mode

--- app/src/main/java/com/example/tudeeapp/presentation/screen/task/TasksScreen.kt
+++ app/src/main/java/com/example/tudeeapp/presentation/screen/task/TasksScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
