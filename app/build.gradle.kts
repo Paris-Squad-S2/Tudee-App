bb846a1f4940a324d0351fd861df463eb20a765b
Mustafa Ibrahim <118114457+MustafaIbrahim96@users.noreply.github.com>
2025-07-02 18:42:03 +0300

    enhancement Ci for test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,7 +115,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    // JUnit 5 API
+    // JUnit 5
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
